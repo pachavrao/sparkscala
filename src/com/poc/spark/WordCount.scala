472006e790dc319c051dc387818f75c04705b43c
venkat <venkateswara.pacha@happiestminds.com>
2020-01-28 13:05:14 +0530

    long line added

--- src/com/poc/spark/WordCount.scala
+++ src/com/poc/spark/WordCount.scala
@@ -16,7 +16,7 @@ object WordCount {
     var a =10
      a = 10
     a=10
-    
+    print("asdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
     val all_lines = sc.textFile("in/word_count.text")
     val words = all_lines.flatMap(line => line.split(" "))
 
